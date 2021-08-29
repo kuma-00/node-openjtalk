beee4ee594656a0b09e9782593624e20bb1d9825
kuma <87855413+kuma-00@users.noreply.github.com>
2021-08-29 06:19:36 +0000

    今風に少し書き換え。 自分に使いやすいように、オプションを変更できるよに。

---
+++ rewrite.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const uuid = require('uuid-v4');
+const fs = require('fs').promises;
+const util = require('util');
+const childProcess = require('child_process');
+const exec = util.promisify(childProcess.exec);
+
+// デフォルトパラメタ
+const DefaultOptions = {
+  openjtalk_bin: path.join(__dirname, '/bin/open_jtalk'),
+  dic_dir: path.join(__dirname, '/dic/open_jtalk_dic_utf_8-1.09'),
+  htsvoice: path.join(__dirname, '/voice/mei/mei_normal.htsvoice'),
+  wav_dir: '.',
+};
+
+class OpenJTalk {
+  constructor(options) {
+    Object.assign(this, DefaultOptions, options)
+  }
+
+  async speak(text, options) {
+    Object.assign(this, options)
+    const wavFileName = uuid() + '.wav';
+    /**
+     * htsvoice
+     * pitch
+     * gv_weight_lf0
+     * additional_half_tone
+     * alpha
+     */
+    let ojtCmd = this.openjtalk_bin;
+    const cmdOptions = {
+      m: this.htsvoice,
+      x: this.dic_dir,
+      s: this.sampling_rate,
+      p: this.pitch,
+      a: this.alpha,
+      b: this.beta,
+      u: this.uv_threshold,
+      jm: this.gv_weight_mgc,
+      jf: this.gv_weight_lf0,
+      z: this.audio_buff_size,
+      fm: this.additional_half_tone,
+      r: this.speech_speed_rate,
+      ow: path.join(this.wav_dir, wavFileName)
+    };
+    for (let option in cmdOptions) {
+      const value = cmdOptions[option];
+      if (value) {
+        ojtCmd += ' -' + option + ' ' + value;
+      }
+    }
+    try {
+      const cmd = 'echo "' + text + '" | ' + ojtCmd;
+      await exec(cmd);
+      const buf = await fs.readFile(wavFileName);
+      fs.unlink(wavFileName);
+      return buf;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = OpenJTalk;
---
+++ test/rewrite/test.js
@@ -0,0 +1,23 @@
+const OpenJTalk = require('../../rewrite.js');
+const mei1 = new OpenJTalk(); // mei_normal を使用
+const mei2 = new OpenJTalk({ htsvoice: './voice/mei/mei_angry.htsvoice' });
+const mei3 = new OpenJTalk({ htsvoice: './voice/mei/mei_happy.htsvoice' });
+
+(async () => {
+  const result = await mei1.speak("これはテストです。");
+  console.log(result);
+})();
+
+/*
+mei1.talk('これはテストです'); // コールバックは省略できる
+setTimeout(function() {
+	mei2.talk('喋り終わり次第コールバックを呼びます', function(err) {
+		if (err) console.log('err', err);
+		console.log('コールバックを呼ぶ');
+		mei3.talk('エラーが発生したらコールバックの引数からチェックできます', function(err) {
+			if (err) console.log('err', err);
+			else mei1.talk('エラーは発生しませんでした');
+		});
+	});
+}, 2000);
+*/
